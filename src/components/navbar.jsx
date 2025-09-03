ef1ba642ac6ad91fbc380cc4ae07d3ebb0c60be5
Wageesha Kumudini <wageesha.e@botcalm.com>
2025-09-03 16:11:11 +0530

    test 4

--- src/components/navbar.jsx
+++ src/components/navbar.jsx
@@ -9,11 +9,8 @@ const Navbar = () => {
 
         <ul className="flex items-center gap-8">
   <li>
-   <a href="#"
-  className="!text-black font-medium hover:!text-sky-600">
-      Home
-   </a>
 
+ //test 4
    <a href="#"
   className="!text-black font-medium hover:!text-sky-600">
       Home
